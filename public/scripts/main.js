eb3e29f3e11fc173a02d3a33287351eee2de72c6
Cameron Nokes <ccnokes@gmail.com>
2015-08-30 20:30:43 -0600

    apply font face asynchronously, disable wercker temporarily

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,35 @@
+var FontFaceOnload = require('./core/onfontfaceload');
+/**
+ * adds a class only once to an element
+ * @param {DOMElement} node
+ * @param {String} newClass - the class to add
+ * @return {DOMElement}
+ */
+function addClassOnce(node, newClass) {
+    let classes = node.className.trim().split(' ');
+    if(classes.indexOf(newClass) === -1) {
+        classes.push(newClass.trim());
+        node.className = classes.join(' ');
+    }
+    return node;
+}
+
+const HEADER_FONT = 'Montserrat';
+const BODY_FONT = 'Hind';
+
+/*jshint newcap:false*/
+FontFaceOnload(HEADER_FONT, {
+    success() {
+        addClassOnce(document.body, 'font-' + HEADER_FONT);
+    }
+});
+
+FontFaceOnload(BODY_FONT, {
+    success() {
+        addClassOnce(document.body, 'font-' + BODY_FONT);
+    }
+});
+
 
 var React = require('react');
 var Router = require('react-router');
